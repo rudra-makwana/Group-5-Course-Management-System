6ebab493c4438a2dd1f285b5ac0d414ce26943b6
krutin47 <krutin47trivedi@gmail.com>
2020-06-02 15:14:21 -0300

    WIP: UPDATED code with testing

---
+++ src/test/java/com/example/group5/service/CsvFileToStudentServiceTest.java
@@ -0,0 +1,15 @@
+package com.example.group5.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CsvFileToStudentServiceTest {
+
+    @Test
+    void loadCsvFileFromURL() {
+
+    }
+}
