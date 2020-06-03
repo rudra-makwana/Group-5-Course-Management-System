d519a27c76a25fc20f7c9a4e94eff44fb83f4f43
Chetanpreet Singh Sachdeva <ch769380@dal.ca>
2020-06-03 16:28:59 -0300

    Added Working Login Page

---
+++ src/main/java/com/example/group5/controller/RegisterController.java
@@ -0,0 +1,105 @@
+package com.example.group5.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.group5.entities.User;
+import com.example.group5.services.LoginService;
+import com.example.group5.services.UserService;
+
+/**
+ * This is controller class for mapping requests with responses
+ * @author Japnoor Kaur
+ * updated by Chetanpreet Singh for login
+ *
+ */
+@Controller
+public class RegisterController {
+
+	//Login code by Chetanpreet Singh
+	@GetMapping("/login")                               //called when/login is called
+	public String loginForm(Model model)
+	{
+		model.addAttribute("user", new User());    //User going to form
+		return "login";
+	}
+	
+	
+
+
+	@PostMapping("/login")
+	public String loginAction(User user, Model model,LoginService loginService)       //User object(From the form submission) and Model object
+	{
+		String response = loginService.findUser(user);
+		return response;
+	}
+
+
+	@GetMapping("/register")
+	public String registerForm(Model model) {
+		model.addAttribute("user", new User());
+		return "register";
+	}
+
+	@PostMapping("/register")
+	public String registerAction(User user, Model model, UserService userService) {
+
+		if(userService.bannerIdExists(user.getBannerId())) 
+		{
+			model.addAttribute("bannerIdAlreadyExists", true);
+			return "register";
+		}
+		
+		
+		if(userService.userExists(user.getEmailID())) 
+		{
+			model.addAttribute("userAlreadyExists", true);
+			return "register";
+
+		}
+		
+		
+		if(userService.validateBannerId(user.getBannerId())) 
+		{
+			model.addAttribute("bannerIdError", true);
+			return "register";
+
+		}
+		
+		
+		if(userService.validateFirstName(user.getFirstName())) 
+		{
+			model.addAttribute("firstNameError", true);
+			return "register";
+
+		}
+		
+		
+		if(userService.validateLastName(user.getLastName())) 
+		{
+			model.addAttribute("lastNameError", true);
+			return "register";
+
+		}
+
+		if(userService.validateEmail(user.getEmailID())) 
+		{
+			model.addAttribute("emailError", true);
+			return "register";
+
+		}
+		if(userService.validatePassword(user.getPassword())) 
+		{
+			model.addAttribute("passwordError", true);
+			return "register";
+
+		}
+
+		else {
+			userService.addUser(user);
+			return "success";
+		}
+	}
+}
---
+++ src/main/java/com/example/group5/services/LoginService.java
@@ -0,0 +1,77 @@
+package com.example.group5.services;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+import com.example.group5.DBConnection;
+import com.example.group5.entities.User;
+
+/*
+ Created by Chetanpreet Singh Sachdeva
+ */
+
+@Service
+public class LoginService  {
+
+	private Connection connect = null;
+	private  Statement statement = null;
+	public String findUser(User user) {
+		String sql ="Select emailID,password,isAdmin from CSCI5308_5_TEST.Users where emailID='"+user.getEmailID()+"' and password='"+user.getPassword()+"';";
+		String rs="";
+		String adminemail = "";
+		String isAdmin = "N";
+		
+		
+		try {
+			
+			//Establishing DB connection
+			DBConnection db =  new DBConnection();
+			connect = db.openConnection();
+			statement = connect.createStatement();
+			statement.executeQuery("use CSCI5308_5_TEST;");
+			ResultSet resultSet = statement.executeQuery(sql);    //Query executed
+
+			if(resultSet.next())            //Fetching results from resultSET
+			{
+				if(resultSet.getString("isAdmin").equals("Y"))     //if user is admin
+					rs = "admin";
+				else
+					rs= "noadmin";                     //if user is not admin
+			}
+			else
+				rs = "loginError";                     //if user is not present
+
+			db.closeConnection(statement,connect);      //close db instance
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		finally {
+			// Always close connections, otherwise the MySQL database runs out of them.
+			// Close any of the resultSet, statements, and connections that are open and holding resources.
+			try {
+
+				if (statement != null) {
+					statement.close();
+				}
+
+				if (connect != null) {
+					connect.close();
+				}
+			} 
+			catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+
+		return rs;
+
+
+	}
+}
