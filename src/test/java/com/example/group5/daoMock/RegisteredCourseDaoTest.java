c9560a4d47273b8234f5cc7ce7762b4cfe2f3130
Rudra Makwana <rudrajmakwana@gmail.com>
2020-06-04 20:37:55 -0300

    Minor bug fixes

---
+++ src/test/java/com/example/group5/daoMock/RegisteredCourseDaoTest.java
@@ -0,0 +1,16 @@
+package com.example.group5.daoMock;
+
+import com.example.group5.model.RegisteredCourses;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RegisteredCourseDaoTest implements com.example.group5.dao.RegisteredCourseDao {
+    List<RegisteredCourses> registeredCoursesList = new ArrayList<>();
+
+    @Override
+    public List<RegisteredCourses> getRegisteredCourse(String bannerID) {
+        registeredCoursesList.add(new RegisteredCourses("CSCI-5308",bannerID,1,"TA","ASDC"));
+        return registeredCoursesList;
+    }
+}
