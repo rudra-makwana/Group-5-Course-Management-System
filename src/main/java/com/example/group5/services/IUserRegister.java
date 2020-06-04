5f87a17979662ba3832878725cf7fd3d063dd79d
krutin47 <krutin47trivedi@gmail.com>
2020-06-04 19:01:19 -0300

    WIP: ADDED Mail all japnoors CODE exception: had no choice to make another branch

---
+++ src/main/java/com/example/group5/services/IUserRegister.java
@@ -0,0 +1,13 @@
+package com.example.group5.services;
+import com.example.group5.model.User;
+
+/**
+ * This is an interface for adding user to database for registration
+ * @author Japnoor Kaur
+ *
+ */
+
+public interface IUserRegister {
+	  public void addUser(User user) ;
+
+}
