2952db39baf9329c9061e22551a6e970fa4aa4b8
Rudra Makwana <rudrajmakwana@gmail.com>
2020-06-04 23:47:16 -0300

    Final changes

--- src/main/java/com/example/group5/services/LoginService.java
+++ src/main/java/com/example/group5/services/LoginService.java
@@ -5,10 +5,12 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.example.group5.model.User;
 import org.springframework.stereotype.Service;
 
 import com.example.group5.DBConnection;
-import com.example.group5.entities.User;
+import com.example.group5.model.User;
 
 /*
  Created by Chetanpreet Singh Sachdeva
